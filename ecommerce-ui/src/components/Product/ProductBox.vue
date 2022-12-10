7f93621d0135bad6d51a13a11d21de525d93763b
siddhanthbakshi <siddhanthbakshi@gmail.com>
2022-12-10 18:37:54 -0500

    Homepage.
    Added Homepage along with the product view page.

--- ecommerce-ui/src/components/Product/ProductBox.vue
+++ ecommerce-ui/src/components/Product/ProductBox.vue
@@ -4,6 +4,8 @@
         <img class="card-img-top embed-responsive-item" :src="product.imageURL" alt="Product Image">
       </div>
       <div class="card-body">
+        <router-link :to="{ name: 'ShowDetails', params: { id : product.id } }"><h5 class="card-title">{{product.name}}</h5></router-link>
+
         <h5 class="card-title">{{product.name}}</h5>
         <p class="card-text"><sup>$</sup>{{product.price}}</p>
         <p class="card-text font-italic">{{product.description.substring(0,65)}}...</p>
