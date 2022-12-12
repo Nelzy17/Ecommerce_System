88537e2d2c2524e89687bb84586ecfc370bd97b7
bhandariketan <bhandariketan38@gmail.com>
2022-12-12 00:38:51 -0500

    Added Review Section in Frontend

--- ecommerce-ui/src/views/Product/ShowDetails.vue
+++ ecommerce-ui/src/views/Product/ShowDetails.vue
@@ -12,9 +12,9 @@
             <div class="col-md-6 col-12 pt-3 pt-md-0">
                 <h4>{{product.productName}}</h4>
 
-                <h6 class="category font-italic">{{category.categoryName}}</h6>
+                <h6 class="category font-italic" id="bottom-space">{{category.categoryName}}</h6>
 
-                <p><span class="font-weight-bold">Description: -</span> <br>{{product.description}}</p>
+                <p id="bottom-space"><span class="font-weight-bold">Description: -</span> <br>{{product.description}}</p>
 
                 <div class="d-flex flex-row justify-content-between">
                     <div class="input-group col-md-3 col-4 p-0">
@@ -28,26 +28,27 @@
                         Add to Cart
                         <ion-icon name="cart-outline" v-pre></ion-icon>
                     </button>
-                
-                
-                
-                </div>
-
-
-                
-
-                <!-- Dummy placeholder features -->
-                <div class="features pt-3">
-                    <h5><strong>Features</strong></h5>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                        <li>Officia quas, officiis eius magni error magnam voluptatem</li>
-                        <li>nesciunt quod! Earum voluptatibus quaerat dolorem doloribus</li>
-                        <li>molestias ipsum ab, ipsa consectetur laboriosam soluta et</li>
-                        <li>ut doloremque dolore corrupti, architecto iusto beatae.</li>
-                    </ul>
                 </div>
             </div>
+        </div>
+        <div class="column pt-5">
+                <div class="features pt-3">
+                    <h5><strong>Reviews</strong></h5>
+                    <ul v-for= "review of reviews" :key="review.id" class="reviewslist">
+                        <li class="reviewItem">{{review.review}} - {{review.userName}}</li>
+                    </ul>
+                </div>
+                <div id="add-Review">
+                    <h5><strong>Add Your Reviews</strong></h5>
+                    <form>
+                        <label> Review: </label>
+                        <textarea v-model="content"></textarea>
+                    </form>
+                    <button type="button" id="add-review-button" class="btn" @click="addReview(this.id, content )">
+                        Add a Review
+                    </button>
+                </div>
+            
         </div>
 
     </div>
@@ -63,7 +64,9 @@ import swal from 'sweetalert';
               product : {},
               category : {},
               id : null,
-              quantity: 1
+              quantity: 1,
+              reviews: null,
+              content:null
           }
       },
       props : ["baseURL","products", "categories"],
@@ -92,6 +95,45 @@ import swal from 'sweetalert';
                 });
             });
         },
+        addReview(productId, rev){
+            // post productId and quantity
+            axios.post(`${this.baseURL}review/add`,{
+                productId : productId,
+                review : rev,
+                userName: this.userName
+            }).then((response) => {
+                // success
+                if(response.status==201){
+                    swal({
+                        text: "Review Added!",
+                        icon: "success",
+                        closeOnClickOutside: false,
+                    });
+                    this.getReviews(productId)
+                    this.content = null
+                }
+            },(error) =>{
+                // error handling
+                console.log(error)
+                swal({
+                    text: "Something wrong with add a Review",
+                    icon: "error",
+                    closeOnClickOutside: false,
+                });
+            });
+        },
+        getReviews(productId){
+            axios.get(`${this.baseURL}/review/?productId=${productId}`).then((response) => {
+                if(response.status==200){
+                const result = response.data;
+                // store cartitems and total price in two variables
+                this.reviews = result;
+                }
+            },
+            (error)=>{
+                console.log(error)
+            });
+        },
 
       },
       mounted() {
@@ -99,6 +141,7 @@ import swal from 'sweetalert';
           this.product = this.products.find(product => product.productId == this.id);
           this.category = this.categories.find(category => category.id == this.product.categoryId);
           this.userName = localStorage.getItem('userName');
+          this.getReviews(this.id)
       }
     }
 </script>
@@ -115,12 +158,41 @@ import swal from 'sweetalert';
         margin: 0;
     }
 
-    /* Firefox */
-    input[type=number] {
-        -moz-appearance: textfield;
-    }
     #add-to-cart-button {
     background-color: #febd69;
-}
+    }
+
+    .reviewslist{
+        text-align: initial;
+    }
+
+    #add-Review{
+        box-sizing: border-box;
+        text-align: initial;
+        margin-top: 50px;
+    }
+
+    label{
+        display: block;
+    }
+
+    input[type="text"],textarea{
+        display: block;
+        width: 100%;
+        padding: 8px
+    }
+
+    #add-review-button{
+        background-color: #febd69;
+        margin: 20px auto;
+    }
+
+    #bottom-space{
+        margin-bottom: 3rem;
+    }
+
+    li{
+        padding-bottom: 0px !important;
+    }
 
 </style>
